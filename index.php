97b259b168229a42c74f5fc551a7e1a4ea52b2e3
Agbooa12 <agboolaelijah077@gmail.com>
2024-05-24 02:32:00 +0100

    some changes

--- index.php
+++ index.php
@@ -35,7 +35,7 @@
           <a class="nav-link" href="./student/Files.php">Upload File</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="./student/display.php" >Display</a>
+          <a class="nav-link" href="./student/display.php" >Display File</a>
         </li>
       <li class="nav-item">
           <a class="nav-link" href="register.php" >Logout</a>
