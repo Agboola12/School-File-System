20fa48803559d801b79ad633a4dcd09ebbe6d3a6
Agbooa12 <agboolaelijah077@gmail.com>
2024-05-24 06:18:17 +0100

    wip to the design

--- index.php
+++ index.php
@@ -31,12 +31,12 @@
         <!-- <li class="nav-item">
           <a class="nav-link " aria-current="page" href="#">Home</a>
         </li> -->
-        <!-- <li class="nav-item">
+        <li class="nav-item">
           <a class="nav-link" href="./student/Files.php">Upload File</a>
-        </li> -->
-<!--         <li class="nav-item">
+        </li>
+        <li class="nav-item">
           <a class="nav-link" href="./student/display.php" >Display Files</a>
-        </li> -->
+        </li>
       <li class="nav-item">
           <a class="nav-link" href="register.php" >Login</a>
       </li>
