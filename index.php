98cc0d4066d6fd067731db18cc43ff6512a28332
Agbooa12 <agboolaelijah077@gmail.com>
2024-05-24 06:04:50 +0100

    wip to the design

--- index.php
+++ index.php
@@ -28,9 +28,9 @@
     </button>
     <div class="collapse navbar-collapse" id="navbarSupportedContent">
       <ul class="navbar-nav me-auto mb-2 mb-lg-0 mx-auto">
-        <li class="nav-item">
+        <<!-- li class="nav-item">
           <a class="nav-link " aria-current="page" href="#">Home</a>
-        </li>
+        </li> -->
         <!-- <li class="nav-item">
           <a class="nav-link" href="./student/Files.php">Upload File</a>
         </li> -->
